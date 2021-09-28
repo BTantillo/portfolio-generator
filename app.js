c57a1f3dbde153e02f1aa0d5ea24a78dae89c5ff
Mountaineer Ben <bentantillo@gmail.com>
2021-09-28 16:19:22 -0700

    generate webpage using command line

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
-const profileDataArgs = process.argv.slice(2)
+const fs = require('fs')
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
 
-const printProfileData = profileDataArr => {
-    // this...
-    for (let i = 0; i < profileDataArr.length; i += 1){
-        console.log(profileDataArr[i]);
-    }
-    console.log('================')
 
-    // is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
 
-printProfileData(profileDataArgs);
+// const printProfileData = profileDataArr => {
+//     // this...
+//     for (let i = 0; i < profileDataArr.length; i += 1){
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('================')
+
+//     // is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
+
+
+
+  fs.writeFile('index.html', generatePage(name, github), err => {
+      if (err) throw new Error(err);
+
+      console.log('Portfolio complete! Check out index.html to see the output!');
+  });
